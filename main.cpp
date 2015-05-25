43b06e6c72680391f156aa9989b7b9c36a063b2d
damimikstest <damimikswebdev@gmail.com>
2015-05-25 19:23:53 +0200

    zrobiłem commita przez przeglądarke w gicie
    Badalambada

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main() {
    	// your code goes here
    	using namespace std;
    	cout << "Welcome to super program" << endl;
+   	cout << "Welcome to super program second line" << endl;
   	return 0;
 }
