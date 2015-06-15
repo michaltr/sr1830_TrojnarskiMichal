9931a81b15a376b78efab51272f6368235bd41b6
michaltr <michaltr.agh@gmail.com>
2015-06-15 18:01:44 +0200

    To ja

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 # Symulator-wyborow
 programik nasz
+=======
+# inpg_election
+>>>>>>> dd6ae90a1ab85bdc519fb2cdaff9ed61b56fc243
---
+++ election1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class candidat
+{
+	string imie;
+	string nazwisko;
+	int age; 
+}
+
+main()
+{
+
+	return;	
+}
+	
